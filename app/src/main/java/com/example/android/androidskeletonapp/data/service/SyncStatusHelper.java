6ceb67030049dfd74a7035d7fc5adbb2a1c6a290
Farai Mutero <fmutero@gmail.com>
2019-07-02 17:08:24 +0200

    Did ex04

--- app/src/main/java/com/example/android/androidskeletonapp/data/service/SyncStatusHelper.java
+++ app/src/main/java/com/example/android/androidskeletonapp/data/service/SyncStatusHelper.java
@@ -1,11 +1,19 @@
 package com.example.android.androidskeletonapp.data.service;
 
+import com.example.android.androidskeletonapp.data.Sdk;
+
+import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.d2manager.D2Manager;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
 public class SyncStatusHelper {
 
     // TODO implement count methods
 
     public static int programCount() {
-        return 0;
+        return Sdk.d2().programModule().programs.count();
     }
 
     public static int dataSetCount() {
